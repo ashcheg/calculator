7ebdf06e2fae9d460ce13596fc7cd6f243eb8550
ashcheg <alina.shcheg@gmail.com>
2022-04-02 13:10:44 -0400

    working prototype, can handle one operation at a time

--- myScript.js
+++ myScript.js
@@ -18,13 +18,59 @@ function divide(x, y) {
 };
 
 function operate(operation, x, y) {
-    return operation(x, y);
+    return (operation==='add') ? add(x, y)
+        : (operation==='subtract') ? subtract(x, y)
+        : (operation==='multiply') ? multiply(x, y)
+        : divide(x, y);
 };
 
-function display(e) {
-    let buttonID = e.target.getAttribute('id');
-    let value = document.getElementById(buttonID).textContent;
-    DISPLAY.textContent = value;
+function display(value) {
+    DISPLAY.textContent += value;
 }
 
-BUTTONS.forEach(button => button.addEventListener('click', display));
+//storing values for later evaluation
+let firstNum = '';
+let secondNum = '';
+let operationSign = '';
+//mode tells us that calculation haven't began yet
+let mode = 0;
+
+BUTTONS.forEach(button => button.addEventListener('click', function() {
+    let buttonID = button.getAttribute('id');
+    let value = document.getElementById(buttonID).textContent;
+    if (buttonID === 'clear') {
+        DISPLAY.textContent = '';
+        firstNum = '';
+        secondNum = '';
+    } else if (buttonID==='add' || buttonID==='subtract' || buttonID==='divide' || buttonID==='multiply') {
+        //pressing operation sign
+        display(value);
+        firstNum = Number(firstNum);
+        operationSign = buttonID;
+        //turn on calculation, waiting for second number
+        mode = 1;
+    
+    } else if (buttonID==='operate') {
+        //pressing equal sign
+        DISPLAY.textContent = '';
+        secondNum = Number(secondNum);
+        DISPLAY.textContent = operate(operationSign, firstNum, secondNum);
+        mode = 0;
+    } else { 
+        //pressing a number
+        display(value);
+        if (mode === 1) {
+            secondNum += value;
+        } else {
+            firstNum += value;
+        }
+    }
+}));
+
+/* what to improve:
+-- make it possible to do multiple operations without pressing equal sign
+(each pair evaluated at a time, not a whole expression);
+-- round numbers with log decimals;
+-- handle pressing '=' before entering all numbers or operator
+-- handle dividing by zero
+*/
