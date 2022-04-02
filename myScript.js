c67c725fe827354343615abe377518170f38527b
ashcheg <alina.shcheg@gmail.com>
2022-04-02 10:33:10 -0400

    added display function

--- myScript.js
+++ myScript.js
@@ -1,3 +1,6 @@
+const DISPLAY = document.getElementById('display');
+const BUTTONS = Array.from(document.querySelectorAll('button'));
+
 function add(x, y) {
     return x+y;
 };
@@ -17,3 +20,12 @@ function divide(x, y) {
 function operate(operation, x, y) {
     return operation(x, y);
 };
+
+function display(value) {
+    DISPLAY.textContent = value;
+}
+
+BUTTONS.forEach(button => button.addEventListener('click', function() {
+    value = button.getAttribute('id');
+    display(value);
+}));
